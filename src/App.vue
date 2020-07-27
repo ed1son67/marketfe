f048544caddafc6ddb0b58c165e86da03dfbef54
ed1son67 <1215373008@qq.com>
2020-07-27 14:10:41 +0800

    feat：加了点reset样式

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,26 @@
 </template>
 
 <style lang="scss">
-html,
-body {
-  margin: 0;
-  padding: 0;
-  height: auto;
-  width: 100%;
-  color: #333;
-}
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    height: auto;
+    width: 100%;
+    color: #333;
+  }
 
-#app {
-  height: auto;
-  width: 100%;
-}
+  #app {
+    height: auto;
+    width: 100%;
+  }
+  button {
+    outline: none;
+    background: transparent;
+    cursor: pointer;
+    border: none;
+  }
+  i {
+    font-style: normal;
+  }
 </style>
