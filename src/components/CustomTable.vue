8f9aadc12fe32518505321df38de3325f58eee15
ed1son67 <1215373008@qq.com>
2020-07-28 13:08:24 +0800

    feat：封装全局table组件

---
+++ src/components/CustomTable.vue
@@ -0,0 +1,54 @@
+<template>
+    <Table
+        border
+        :columns="columnsComputed"
+        :data="data"
+    >
+        <template slot="action" slot-scope="">
+            <Button type="error" style="margin-right: 5px" @click="handleDelete">删除</Button>
+        </template>
+    </Table>
+</template>
+
+<script>
+    import { Table, Button } from 'view-design';
+    export default {
+        name: 'CustomTable',
+        components: {
+            Button,
+            Table
+        },
+        props: {
+            columns: {
+                type: Array,
+                required: true
+            },
+            data: {
+                type: Array,
+                required: true
+            }
+        },
+        data () {
+          return {
+              actionColumn: {
+                  title: '操作',
+                  slot: 'action',
+                  width: 150,
+                  align: 'center'
+              }
+          };
+        },
+        computed: {
+            columnsComputed () {
+                return [...this.columns, this.actionColumn];
+            }
+        },
+        methods: {
+            handleDelete (e) {
+                this.$emit('delete', e);
+            }
+        }
+    };
+</script>
+
+<style lang="scss"></style>
---
+++ src/pages/User/User.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="page">
+        <div>
+            <div>所有用户信息：</div>
+            <CustomTable
+              :columns="columns"
+              :data="users"
+              @delete="handleDelete"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+    import CustomTable from '../../components/CustomTable';
+    import types from '../../store/types';
+    import { mapState } from 'vuex';
+    export default {
+        name: 'User',
+        components: {
+            CustomTable
+        },
+        data () {
+            return {
+                columns: [{
+                    title: 'ID',
+                    key: 'id'
+                }, {
+                    title: '用户名',
+                    key: 'username'
+                }],
+            };
+        },
+        computed: {
+            ...mapState({
+                users: 'users'
+            })
+        },
+        created () {
+            // 拉取用户数据
+            this.$store.dispatch(types.GET_USERS);
+        },
+        methods: {
+            handleDelete (e) {
+                console.log(e);
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+</style>
